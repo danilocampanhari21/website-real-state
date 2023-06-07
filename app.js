9d74e0f8a911af41717aba2a1954308f494290f9
danilocampanhari21 <danilocampanhari64@gmail.com>
2023-06-07 16:22:30 +0200

    ...

--- app.js
+++ app.js
@@ -3,5 +3,8 @@ const inputs = document.querySelectorAll(".contact-input");
 inputs.forEach(ipt => {
     ipt.addEventListener("focus", () => {
         ipt.parentNode.classList.add("focus");
-    })
-})
+    });
+    ipt.addEventListener("blur", () => {
+        ipt.parentNode.classList.remove("focus");
+    });
+});
